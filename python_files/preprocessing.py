4affa6de62c4de8a51add63185c58662b4feba91
bartvanvulpen99 <bartvanvulpen@icloud.com>
2018-06-05 00:39:18 +0200

    added small testing set, renamed full raw dataset

--- python_files/preprocessing.py
+++ python_files/preprocessing.py
@@ -2,15 +2,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import zipfile
+import sys
 
-zf = zipfile.ZipFile('datasets/stage3.csv.zip') 
+
+
+zf = zipfile.ZipFile('datasets/stage3.csv.zip')
 data = pd.read_csv(zf.open('stage3.csv'))
 
 # function to delete column if more than 40% of the values is unknowm
 total_count = len(data)
+
 for column in data.columns:
     items = data[column]
-    count = items.isnull().sum()  
+    count = items.isnull().sum()
     percentage = (count / total_count) * 100
     print(column, "=", percentage)
     if percentage >= 40.0:
