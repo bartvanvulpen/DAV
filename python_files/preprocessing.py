7b27abc527c764af20c16273f32d580c8798926f
bartvanvulpen99 <bartvanvulpen@icloud.com>
2018-06-05 01:10:09 +0200

    refined data structure of incident_characteristics column

--- python_files/preprocessing.py
+++ python_files/preprocessing.py
@@ -3,11 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import zipfile
 import sys
+import math
 
+# unzipping and reading full dataset
+#zf = zipfile.ZipFile('DAV/datasets/stage3.csv.zip')
+#data = pd.read_csv(zf.open('stage3.csv'))
 
-
-zf = zipfile.ZipFile('datasets/stage3.csv.zip')
-data = pd.read_csv(zf.open('stage3.csv'))
+# testing functions with smaller dataset
+data = pd.read_csv('DAV/datasets/small_test_set.csv')
 
 # function to delete column if more than 40% of the values is unknowm
 total_count = len(data)
@@ -16,6 +19,23 @@ for column in data.columns:
     items = data[column]
     count = items.isnull().sum()
     percentage = (count / total_count) * 100
-    print(column, "=", percentage)
+    #print(column, "=", percentage)
     if percentage >= 40.0:
         data = data.drop(column, 1)
+        
+# bewerkingen aan incidents_characteristics kolom        
+data["incident_characteristics"] = data["incident_characteristics"].fillna('shot')
+data["incident_characteristics"] = data["incident_characteristics"].str.lower()
+for item in data["incident_characteristics"]:
+    item = item.replace(" -", ",")
+    item = item.replace(" and/or", ",")
+    item = item.replace("/", ",")
+    item = item.replace("||", ",")
+    item = item.replace("gun(s)", "guns")
+    item = item.replace(" (", ", ")
+    item = item.replace(", ", ",")
+    item = item.replace(")", "")
+    item = item.replace("suicide^", "suicide")
+    print(item)
+# write processed dataframe to CSV
+# data.to_csv('DAV/datasets/full_dataset_clean.csv', sep=",")
