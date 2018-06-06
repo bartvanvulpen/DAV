9be946f27f40d988e38d9c438fa51b04b0c570b1
bartvanvulpen99 <bartvanvulpen@icloud.com>
2018-06-06 13:09:28 +0200

    removed other irrelevant columns
    Co-Authored-By: ctselcuk <cem.t.selcuk@gmail.com>

--- python_files/preprocessing.py
+++ python_files/preprocessing.py
@@ -8,7 +8,9 @@ from pandas import ExcelWriter
 
 # unzipping and reading full dataset
 zf = zipfile.ZipFile('datasets/full_dataset_raw.csv.zip')
+
 data = pd.read_csv(zf.open('stage3.csv'))
+
 
 # testing functions with smaller dataset
 #data = pd.read_csv('datasets/small_test_set.csv')
@@ -20,10 +22,19 @@ for column in data.columns:
     items = data[column]
     count = items.isnull().sum()
     percentage = (count / total_count) * 100
-    print(column, "=", percentage)
+    #print(column, "=", percentage)
     if percentage >= 40.0:
         data = data.drop(column, 1)
-
+# remove other irrelevent columns
+data = data.drop("address", 1)
+data = data.drop("incident_url", 1)
+data = data.drop("source_url", 1)
+data = data.drop("incident_url_fields_missing", 1)
+data = data.drop("congressional_district", 1)
+data = data.drop("sources", 1)
+data = data.drop("state_house_district", 1)
+data = data.drop("state_senate_district", 1)
+data = data.drop("notes", 1)
 # bewerkingen aan incidents_characteristics kolom
 data["incident_characteristics"] = data["incident_characteristics"].fillna('shot')
 data["incident_characteristics"] = data["incident_characteristics"].str.lower()
@@ -41,7 +52,8 @@ for item in data["incident_characteristics"]:
     new_column.append(item)
 data["incident_characteristics"] = new_column
 
- #write processed dataframe to Excel testing sheet
+#write processed dataframe to Excel testing sheet
 writer = ExcelWriter('datasets/full_dataset_excel_testing.xlsx')
 data.to_excel(writer,'testingsheet')
 writer.save()
+print("Written to Excel")
