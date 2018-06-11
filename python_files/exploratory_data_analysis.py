98f48026413c262e4837c8684c02b1508cb4f9ea
Thymen Heinsbroek <thymenheinsbroek@gmail.com>
2018-06-11 11:40:46 +0200

    file aangemaakt voor Exploratory data analysis

---
+++ python_files/exploratory_data_analysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import zipfile
+import sys
+from sklearn import preprocessing
+import math
+from pandas import ExcelWriter
+import re
