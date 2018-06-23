6e1992e2a96aae46208c661eef60ab63698b9738
bartvanvulpen99 <bartvanvulpen@icloud.com>
2018-06-24 00:00:37 +0200

    added TSNE and PCA dimension reducers for KMeans + added gmaps map

--- python_files/kmeans.py
+++ python_files/kmeans.py
@@ -7,7 +7,14 @@ from sklearn import preprocessing
 from sklearn import datasets
 import pandas as pd
 import numpy as np
+
 from pandas import ExcelWriter
+from tsne import tsne
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn import metrics
+from scipy.spatial.distance import cdist
+import random
 print("Starting reading data...")
 data = pd.read_excel('datasets/MASTER_DATASET.xlsx')
 
@@ -80,43 +87,126 @@ data['state'] = le.fit_transform(data['state'])
 
 
 
-print(len(population_list))
-print('hello')
+print('Starting KMeans, first reducing dimensions')
 
-#--------------------K-Means 3 clusters-----------------------
+#--------------------KMEANS CLUSTERING-----------------------
 # dropping columns
-data1 = data.drop(["date","city_or_county","state", "latitude", "n_injured","incident_id", "suicide", "participant_gender_involved", "domestic_violence", "home_invasion", "drug_involvement", "officer_involved", "dgu_evidence", "gang_involvement", "adults_involved", "teens_involved", "children_involved"], axis =1)
-print(data1.head())
+data1 = data.drop("incident_id", axis =1)
 
+#----Training and Testing set definition----
+# Using trainingset
+data1 = data1.sample(frac=0.8)
+print(len(data1))
+data1 = data1.drop(data1.index[20000:239398])
 
+# Using testingset
+#data1 = data1.sample(frac=0.2)
+#print(len(data1))
 
+print("Reducing with t-SNE...")
 
-km = KMeans(n_clusters=2, init='k-means++', n_init=10)
-km.fit(data1)
-x = km.fit_predict(data1)
+## reduce dimensions using TSNE
+model = TSNE(n_components=2, random_state=0)
+res = model.fit_transform(data1)
+tsne_data = pd.DataFrame(res[0:, 0:,])
+print("Done")
 
-data["cluster"]= x
-color_list = []
-for cluster in data['cluster']:
-    if cluster == 0:
-        color_list.append('r')
-    if cluster == 1:
-        color_list.append('b')
-    if cluster == 2:
-        color_list.append('g')
-data['color'] = color_list
-
-print(data.head())
-# printing sorted dataset on cluster
-data1 = data.sort_values(['cluster'])
-print(data1)
-plt.scatter(data['n_killed'], data['state_population'], color=data['color'], alpha=0.5, edgecolor='k')
+plt.scatter(tsne_data[0] , tsne_data[1], s=1,c='g', alpha=0.2)
+plt.title("t-SNE")
 plt.show()
 
-# print("Preprocessing successful!")
-# print("Writing output to .xlsx file...")
-# # write processed dataframe to Excel testing sheet
-# writer = ExcelWriter('datasets/kmeans_testing.xlsx')
-# data1.to_excel(writer,'testingsheet')
-# writer.save()
-# print("Output written to .xlsx file!")
+print("Reducing with PCA...")
+
+## Reduce dimensions using PCA
+pca = PCA(n_components=2)
+pca_result = pca.fit_transform(data1)
+pca_data = pd.DataFrame(pca_result[0:, 0:,])
+plt.scatter(pca_data[0] , pca_data[1], s=1,c='r', alpha=0.2 )
+plt.title("PCA")
+plt.show()
+print("Done")
+
+
+#-----------------------KMEANS TSNE_DATA-----------------
+print("KMeans t-SNE...")
+# k means determine k for tsne_data using elbow method
+X = tsne_data
+distortions = []
+K = range(1,10)
+for k in K:
+    kmeanModel = KMeans(n_clusters=k).fit(X)
+    kmeanModel.fit(X)
+    distortions.append(sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+
+# Plot the elbow
+plt.plot(K, distortions, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Distortion')
+plt.title('The Elbow Method showing the optimal k for tsne_data')
+plt.show()
+
+## Cluster dimension-reduced data with KMeans, with n_cluster equal to k
+
+K = 4
+km = KMeans(n_clusters=K, init='k-means++', n_init=100)
+km.fit(tsne_data)
+x = km.fit_predict(tsne_data)
+
+# plot tsne_dat
+tsne_data["cluster"] = x
+color_list = []
+for cluster in tsne_data['cluster']:
+    if cluster == 0:
+        color_list.append('red')
+    if cluster == 1:
+        color_list.append('green')
+    if cluster == 2:
+        color_list.append('blue')
+    if cluster == 3:
+        color_list.append('orange')
+tsne_data['color'] = color_list
+
+
+plt.scatter(tsne_data[0], tsne_data[1], s=1,c=tsne_data['color'], alpha=0.2)
+plt.show()
+
+#-----------------------KMEANS PCA_DATA-----------------
+print("KMeans PCA...")
+# k means determine k for pca_data usign elbow method
+X = pca_data
+distortions = []
+K = range(1,10)
+for k in K:
+    kmeanModel = KMeans(n_clusters=k).fit(X)
+    kmeanModel.fit(X)
+    distortions.append(sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+
+# Plot the elbow
+plt.plot(K, distortions, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Distortion')
+plt.title('The Elbow Method showing the optimal k for pca_data')
+plt.show()
+
+## Cluster dimension-reduced data with KMeans, with n_cluster equal to k
+
+K = 2
+km = KMeans(n_clusters=K, init='k-means++', n_init=100)
+km.fit(pca_data)
+x = km.fit_predict(pca_data)
+
+# plot tsne_dat
+pca_data["cluster"] = x
+color_list = []
+for cluster in pca_data['cluster']:
+    if cluster == 0:
+        color_list.append('blue')
+    if cluster == 1:
+        color_list.append('orange')
+
+pca_data['color'] = color_list
+
+
+plt.scatter(pca_data[0], pca_data[1], s=1,c=pca_data['color'], alpha=0.2)
+plt.show()
+print("Finished")
--- python_files/testgraphs.py
+++ python_files/testgraphs.py
@@ -2,260 +2,273 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import zipfile
+import sys
+from sklearn import preprocessing
+import math
+from pandas import ExcelWriter
+import re
+from pandas import ExcelFile
+from geopy import geocoders
+import time
+from bokeh.io import output_file, show
+from bokeh.models import ColumnDataSource, GMapOptions
+from bokeh.plotting import gmap
+from pylab import figure, axes, pie, title, show
+# #
 #
-#Graph numbers killed split by year
+# #Graph numbers killed split by year
+# #
 #
-
-zf = zipfile.ZipFile('datasets/full_dataset_raw.csv.zip')
-
-data = pd.read_csv(zf.open('stage3.csv'))
-#split n_killed numbers based on year
-
-kills_14 = 0
-kills_15 = 0
-kills_16 = 0
-kills_17 = 0
-kills_18 = 0
-for date, kills in zip(data4["date"], data4["n_killed"]):
-    if "2014" in date:
-        kills_14 = kills_14 + kills
-    if "2015" in date:
-        kills_15 = kills_15 + kills
-    if "2016" in date:
-        kills_16 = kills_16 + kills
-    if "2017" in date:
-        kills_17 = kills_17 + kills
-    if "2018" in date:
-        kills_18 = kills_18 + kills
-print(kills_14)
-print(kills_15)
-print(kills_16)
-print(kills_17)
-print(kills_18)
-
-#instantiate x and y
-objects = ('2014', '2015', '2016', '2017', '2018')
-y_pos = np.arange(len(objects))
-performance = [kills_14,kills_15,kills_16,kills_17,kills_18]
-
-#plot graph
-plt.bar(y_pos, performance, align='center', color='#20CA23')
-plt.xticks(y_pos, objects)
-plt.ylabel('Deaths')
-plt.title('Deaths by year')
-plt.show()
-
+# zf = zipfile.ZipFile('datasets/full_dataset_raw.csv.zip')
 #
-#Graph numbers injured split by year
+# data = pd.read_csv(zf.open('stage3.csv'))
+# #split n_killed numbers based on year
 #
-
-#split n_injured numbers based on year
-injured_14 = 0
-injured_15 = 0
-injured_16 = 0
-injured_17 = 0
-injured_18 = 0
-for date, injured in zip(data4["date"], data4["n_injured"]):
-    if "2014" in date:
-        injured_14 = injured_14 + injured
-    if "2015" in date:
-        injured_15 = injured_15 + injured
-    if "2016" in date:
-        injured_16 = injured_16 + injured
-    if "2017" in date:
-        injured_17 = injured_17 + injured
-    if "2018" in date:
-        injured_18 = injured_18 + injured
-print(injured_14)
-print(injured_15)
-print(injured_16)
-print(injured_17)
-print(injured_18)
-
-#instantiate x and y
-objects = ('2014', '2015', '2016', '2017', '2018')
-y_pos = np.arange(len(objects))
-performance = [injured_14,injured_15,injured_16,injured_17,injured_18]
-
-#plot graph
-plt.bar(y_pos, performance, align='center', color='#20CA23')
-plt.xticks(y_pos, objects)
-plt.ylabel('Injuries')
-plt.title('Injuries by year')
-plt.show()
-
+# kills_14 = 0
+# kills_15 = 0
+# kills_16 = 0
+# kills_17 = 0
+# kills_18 = 0
+# for date, kills in zip(data4["date"], data4["n_killed"]):
+#     if "2014" in date:
+#         kills_14 = kills_14 + kills
+#     if "2015" in date:
+#         kills_15 = kills_15 + kills
+#     if "2016" in date:
+#         kills_16 = kills_16 + kills
+#     if "2017" in date:
+#         kills_17 = kills_17 + kills
+#     if "2018" in date:
+#         kills_18 = kills_18 + kills
+# print(kills_14)
+# print(kills_15)
+# print(kills_16)
+# print(kills_17)
+# print(kills_18)
 #
-# Graph that combines injured and killed numbers
+# #instantiate x and y
+# objects = ('2014', '2015', '2016', '2017', '2018')
+# y_pos = np.arange(len(objects))
+# performance = [kills_14,kills_15,kills_16,kills_17,kills_18]
 #
-
-n_groups = 5
-killed = (kills_14, kills_15, kills_16, kills_17, kills_18)
-injured = (injured_14, injured_15, injured_16, injured_17, injured_18)
-
-# plot graph
-fig, ax = plt.subplots()
-index = np.arange(n_groups)
-bar_width = 0.35
-opacity = 0.8
-
-rects1 = plt.bar(index, killed, bar_width,
-                 alpha=opacity,
-                 color='#268328',
-                 label='Killed')
-
-rects2 = plt.bar(index + bar_width, injured, bar_width,
-                 alpha=opacity,
-                 color='#20CA23',
-                 label='Injured')
-plt.xlabel('Year')
-plt.title('Kills and injuries by year')
-plt.xticks(index + bar_width/2, ('2014', '2015', '2016', '2017', '2018'))
-
-plt.legend()
-plt.tight_layout()
-plt.show()
-fig = plt.figure()
-
+# #plot graph
+# plt.bar(y_pos, performance, align='center', color='#20CA23')
+# plt.xticks(y_pos, objects)
+# plt.ylabel('Deaths')
+# plt.title('Deaths by year')
+# plt.show()
 #
-# Graph gives number of times certain specific conditions apply to incidents
+# #
+# #Graph numbers injured split by year
+# #
 #
-
-#instantiate x and y
-objects = ('dead', 'murder', 'suicide', 'robbery', 'officer involved', 'drug involvement', 'home invasion', 'domestic violence')
-y_pos = np.arange(len(objects))
-
-#these numbers where given by earlier code written in preprocessing.py
-performance = [53685,53281,53357,19495,17738, 16962, 10441, 6008]
-plt.bar(y_pos, performance, align='center', color='#20CA23')
-plt.xticks(y_pos, objects)
-plt.title('Specifications gun violence')
-fig.autofmt_xdate()
-plt.show()
-
+# #split n_injured numbers based on year
+# injured_14 = 0
+# injured_15 = 0
+# injured_16 = 0
+# injured_17 = 0
+# injured_18 = 0
+# for date, injured in zip(data4["date"], data4["n_injured"]):
+#     if "2014" in date:
+#         injured_14 = injured_14 + injured
+#     if "2015" in date:
+#         injured_15 = injured_15 + injured
+#     if "2016" in date:
+#         injured_16 = injured_16 + injured
+#     if "2017" in date:
+#         injured_17 = injured_17 + injured
+#     if "2018" in date:
+#         injured_18 = injured_18 + injured
+# print(injured_14)
+# print(injured_15)
+# print(injured_16)
+# print(injured_17)
+# print(injured_18)
 #
-# Graph splits incidents by state
+# #instantiate x and y
+# objects = ('2014', '2015', '2016', '2017', '2018')
+# y_pos = np.arange(len(objects))
+# performance = [injured_14,injured_15,injured_16,injured_17,injured_18]
 #
-
-word_list = data4["state"]
-from collections import Counter
-words_to_count = (word for word in word_list if word[:1].isupper())
-states = Counter(words_to_count)
-#take top 10 states with most incidents
-top_10 = (states.most_common(10))
-
-states = []
-numbers = []
-for element in top_10:
-    #add name of state and number of incidents to x and y respectively
-    states.append(element[0])
-    numbers.append(element[1])
-
-#plot graph
-fig = plt.figure()
-y_pos = np.arange(len(states))
-plt.bar(y_pos, numbers, align='center', color='#20CA23')
-plt.xticks(y_pos, states)
-plt.ylabel('Incidents')
-plt.title('Incidents per state')
-fig.autofmt_xdate()
-plt.show()
-
-# Comments: 
-# Grafiek 1, Deaths by year:
-# Er is duidelijk een stijgende lijn te zien in de doden per jaar van 2014 tot 2017.
-# Er is weinig te zeggen over 2018, doordat er alleen data is van de eerste paar maanden. 
-# 
-# Grafiek 2, Injured by year: Eenzelfde stijgende lijn als in grafiek 1 is hier ook zichtbaar.
-# 
-# Grafiek 3, grafiek 1 en 2 in een grafiek: Als de grafieken met elkaar vergeleken worden,
-# wordt duidelijk dat de verhouding doden/gewonden ongeveer gelijk blijft, zo'n 1:2.
-# 
-# Grafiek 4, aantallen specificaties: In de dataset is aanvullende informatie gegeven
-# over specifieke incidenten. Deze zijn ingedeeld in een aantal vaste woorden die het vaakst voorkomen.
-# 
-# Grafiek 5, incidenten per staat: Hier zijn de top 10 landen met meeste incidenten aangegeven.
-# Interessant om hier te onderzoeken is waarom illinois en california bijvoorbeeld specifiek zo
-# zo veel incidenten hebben, komt dat door inwonersaantallen of zijn er andere elementen die meespelen?
+# #plot graph
+# plt.bar(y_pos, performance, align='center', color='#20CA23')
+# plt.xticks(y_pos, objects)
+# plt.ylabel('Injuries')
+# plt.title('Injuries by year')
+# plt.show()
 #
-
-allstates = []
-allnumbers = []
-
-for element in all_states:
-    allstates.append(element[0])
-    allnumbers.append(element[1])
-
-#searched up data of population of individual states
-d ={'California': 39144818,
-'Texas': 27469114,
-'Florida':20271272,
-'New York': 19795791,
-'Illinois':12859995,
-'Pennsylvania':12802503,
-'Ohio':11613423,
-'Georgia':10214860,
-'North Carolina':10042802,
-'Michigan':9922576,
-'New Jersey':8958013,
-'Virginia':8382993,
-'Washington':7170351,
-'Arizona':6828065,
-'Massachusetts':6794422,
-'Indiana':6619680,
-'Tennessee':6600299,
-'Missouri':6083672,
-'Maryland':6006401,
-'Wisconsin':5771337,
-'Minnesota':5489594,
-'Colorado':5456574,
-'South Carolina':4896146,
-'Alabama':4858979,
-'Louisiana':4670724,
-'Kentucky':4425092,
-'Oregon':4028977,
-'Oklahoma':3911338,
-'Connecticut':3590886,
-'Iowa':3123899,
-'Utah':2995919,
-'Mississippi':2992333,
-'Arkansas':2978204,
-'Kansas':2911641,
-'Nevada':2890845,
-'New Mexico':2085109,
-'Nebraska':1896190,
-'West Virginia':1844128,
-'Idaho':1654930,
-'Hawaii':1431603,
-'New Hampshire':1330608,
-'Maine':1329328,
-'Rhode Island':1056298,
-'Montana':1032949,
-'Delaware':945934,
-'South Dakota':858469,
-'North Dakota':756927,
-'Alaska':738432,
-'Vermont':626042,
-'Wyoming':586107,
-'District of Columbia': 693972}
-
-population = []
-#couple amount of incidents of state to population of state
-for x in allstates:
-    for y in d:
-        if x == y:
-            population.append(d[y])
-
-#plot graph
-x = population
-y = allnumbers
-plt.scatter(x,y)
-z = np.polyfit(x, y, 1)
-p = np.poly1d(z)
-plt.plot(x,p(x),"r--")
-plt.show()
+# #
+# # Graph that combines injured and killed numbers
+# #
+#
+# n_groups = 5
+# killed = (kills_14, kills_15, kills_16, kills_17, kills_18)
+# injured = (injured_14, injured_15, injured_16, injured_17, injured_18)
+#
+# # plot graph
+# fig, ax = plt.subplots()
+# index = np.arange(n_groups)
+# bar_width = 0.35
+# opacity = 0.8
+#
+# rects1 = plt.bar(index, killed, bar_width,
+#                  alpha=opacity,
+#                  color='#268328',
+#                  label='Killed')
+#
+# rects2 = plt.bar(index + bar_width, injured, bar_width,
+#                  alpha=opacity,
+#                  color='#20CA23',
+#                  label='Injured')
+# plt.xlabel('Year')
+# plt.title('Kills and injuries by year')
+# plt.xticks(index + bar_width/2, ('2014', '2015', '2016', '2017', '2018'))
+#
+# plt.legend()
+# plt.tight_layout()
+# plt.show()
+# fig = plt.figure()
+#
+# #
+# # Graph gives number of times certain specific conditions apply to incidents
+# #
+#
+# #instantiate x and y
+# objects = ('dead', 'murder', 'suicide', 'robbery', 'officer involved', 'drug involvement', 'home invasion', 'domestic violence')
+# y_pos = np.arange(len(objects))
+#
+# #these numbers where given by earlier code written in preprocessing.py
+# performance = [53685,53281,53357,19495,17738, 16962, 10441, 6008]
+# plt.bar(y_pos, performance, align='center', color='#20CA23')
+# plt.xticks(y_pos, objects)
+# plt.title('Specifications gun violence')
+# fig.autofmt_xdate()
+# plt.show()
+#
+# #
+# # Graph splits incidents by state
+# #
+#
+# word_list = data4["state"]
+# from collections import Counter
+# words_to_count = (word for word in word_list if word[:1].isupper())
+# states = Counter(words_to_count)
+# #take top 10 states with most incidents
+# top_10 = (states.most_common(10))
+#
+# states = []
+# numbers = []
+# for element in top_10:
+#     #add name of state and number of incidents to x and y respectively
+#     states.append(element[0])
+#     numbers.append(element[1])
+#
+# #plot graph
+# fig = plt.figure()
+# y_pos = np.arange(len(states))
+# plt.bar(y_pos, numbers, align='center', color='#20CA23')
+# plt.xticks(y_pos, states)
+# plt.ylabel('Incidents')
+# plt.title('Incidents per state')
+# fig.autofmt_xdate()
+# plt.show()
+#
+# # Comments:
+# # Grafiek 1, Deaths by year:
+# # Er is duidelijk een stijgende lijn te zien in de doden per jaar van 2014 tot 2017.
+# # Er is weinig te zeggen over 2018, doordat er alleen data is van de eerste paar maanden.
+# #
+# # Grafiek 2, Injured by year: Eenzelfde stijgende lijn als in grafiek 1 is hier ook zichtbaar.
+# #
+# # Grafiek 3, grafiek 1 en 2 in een grafiek: Als de grafieken met elkaar vergeleken worden,
+# # wordt duidelijk dat de verhouding doden/gewonden ongeveer gelijk blijft, zo'n 1:2.
+# #
+# # Grafiek 4, aantallen specificaties: In de dataset is aanvullende informatie gegeven
+# # over specifieke incidenten. Deze zijn ingedeeld in een aantal vaste woorden die het vaakst voorkomen.
+# #
+# # Grafiek 5, incidenten per staat: Hier zijn de top 10 landen met meeste incidenten aangegeven.
+# # Interessant om hier te onderzoeken is waarom illinois en california bijvoorbeeld specifiek zo
+# # zo veel incidenten hebben, komt dat door inwonersaantallen of zijn er andere elementen die meespelen?
+# #
+#
+# allstates = []
+# allnumbers = []
+#
+# for element in all_states:
+#     allstates.append(element[0])
+#     allnumbers.append(element[1])
+#
+# #searched up data of population of individual states
+# d ={'California': 39144818,
+# 'Texas': 27469114,
+# 'Florida':20271272,
+# 'New York': 19795791,
+# 'Illinois':12859995,
+# 'Pennsylvania':12802503,
+# 'Ohio':11613423,
+# 'Georgia':10214860,
+# 'North Carolina':10042802,
+# 'Michigan':9922576,
+# 'New Jersey':8958013,
+# 'Virginia':8382993,
+# 'Washington':7170351,
+# 'Arizona':6828065,
+# 'Massachusetts':6794422,
+# 'Indiana':6619680,
+# 'Tennessee':6600299,
+# 'Missouri':6083672,
+# 'Maryland':6006401,
+# 'Wisconsin':5771337,
+# 'Minnesota':5489594,
+# 'Colorado':5456574,
+# 'South Carolina':4896146,
+# 'Alabama':4858979,
+# 'Louisiana':4670724,
+# 'Kentucky':4425092,
+# 'Oregon':4028977,
+# 'Oklahoma':3911338,
+# 'Connecticut':3590886,
+# 'Iowa':3123899,
+# 'Utah':2995919,
+# 'Mississippi':2992333,
+# 'Arkansas':2978204,
+# 'Kansas':2911641,
+# 'Nevada':2890845,
+# 'New Mexico':2085109,
+# 'Nebraska':1896190,
+# 'West Virginia':1844128,
+# 'Idaho':1654930,
+# 'Hawaii':1431603,
+# 'New Hampshire':1330608,
+# 'Maine':1329328,
+# 'Rhode Island':1056298,
+# 'Montana':1032949,
+# 'Delaware':945934,
+# 'South Dakota':858469,
+# 'North Dakota':756927,
+# 'Alaska':738432,
+# 'Vermont':626042,
+# 'Wyoming':586107,
+# 'District of Columbia': 693972}
+#
+# population = []
+# #couple amount of incidents of state to population of state
+# for x in allstates:
+#     for y in d:
+#         if x == y:
+#             population.append(d[y])
+#
+# #plot graph
+# x = population
+# y = allnumbers
+# plt.scatter(x,y)
+# z = np.polyfit(x, y, 1)
+# p = np.poly1d(z)
+# plt.plot(x,p(x),"r--")
+# plt.show()
 
 # Waarnemingen: Het plotten van de grafiek geeft niet heel duidelijke resultaten.
-# De staten met de meeste incidenten zijn vrijwel uitsluitend grote staten, maar hier zijn wel veel verschillen in. 
+# De staten met de meeste incidenten zijn vrijwel uitsluitend grote staten, maar hier zijn wel veel verschillen in.
 # Zo is illinois met 12859995 een stuk lager qua inwonersaantal dan california met 39144818, maar illinois heeft meer incidenten.
 
 print("Starting reading data...")
@@ -264,7 +277,7 @@ data = pd.read_excel('datasets/MASTER_DATASET.xlsx')
 
 #
 # Incidenten per maand
-# 
+#
 
 jan = 0
 feb = 0
@@ -305,8 +318,8 @@ for date in data["date"]:
     if "12" in date:
         dec=dec+1
 
-        
-#plot graph 
+
+#plot graph
 fig = plt.figure()
 objects = ('jan','feb','mar','apr','mei','jun','jul','aug','sep','okt','nov','dec')
 y_pos = np.arange(len(objects))
@@ -321,8 +334,8 @@ plt.show()
 
 #Incidenten per maand
 #
-#Meeste incidenten vinden plaats in januari en maart. Hierna zijn de hoogste waarden juist in de zomer(juli en augustus) en zijn december en november het laagste. 
-#Wintermaanden kan misschien verklaard worden met de natuurlijke associatie van het koude weer met depressie, 
+#Meeste incidenten vinden plaats in januari en maart. Hierna zijn de hoogste waarden juist in de zomer(juli en augustus) en zijn december en november het laagste.
+#Wintermaanden kan misschien verklaard worden met de natuurlijke associatie van het koude weer met depressie,
 #Algemeen wordt hierdoor veronderstelt dat er meer zelfmoorden/zelfmoordpogingen in de winter zijn. Februari is hierbij
 #waarschijnlijk lager omdat deze maand een paar dagen korter is.
 #Deze incidenten zijn echter niet uitsluitend suicide-situaties, dus het is moeilijk te zeggen in hoeverre dit van toepassing is.
@@ -392,42 +405,50 @@ for date, killed, injured in zip(data["date"], data["n_killed"], data["n_injured
     if "12" in date:
         dec_killed=dec_killed+killed
         dec_injured = dec_injured+injured
-        
 
-        
+
+
 #plot graph
 n_groups = 12
 killed = (jan_killed, feb_killed, mar_killed, apr_killed, mei_killed,jun_killed,jul_killed, aug_killed, sep_killed, okt_killed, nov_killed, dec_killed)
 injured = (jan_injured, feb_injured, mar_injured, apr_injured, mei_injured, jun_injured, jul_injured, aug_injured, sep_injured, okt_injured, nov_injured, dec_injured )
 print(killed)
 print(injured)
+ratio_list = [(killed*1.0)/injured for killed, injured in zip(killed, injured)]
 fig, ax = plt.subplots()
 index = np.arange(n_groups)
 bar_width = 0.35
 opacity = 0.8
- 
+
 rects1 = plt.bar(index, killed, bar_width,
                  alpha=opacity,
                  color='#268328',
                  label='Killed')
- 
+
 rects2 = plt.bar(index + bar_width, injured, bar_width,
                  alpha=opacity,
                  color='#20CA23',
                  label='Injured')
- 
-plt.xlabel('Year')
+
+plt.xlabel('Months')
 plt.title('Kills and injuries by month')
 plt.xticks(index + bar_width/2, ('jan','feb','mar','apr','mei','jun','jul','aug','sep','okt','nov','dec'))
 plt.legend()
- 
+
 plt.tight_layout()
 plt.show()
 
+#plot graph
+
+fig = plt.figure()
+objects = ['jan','feb','mar','apr','mei','jun','jul','aug','sep','okt','nov','dec']
+plt.plot(objects, ratio_list)
+plt.title('Killed/injured ratio by month')
+plt.show()
 
 #Kills/Injuries per maand
 #
 #Opvallend andere indeling dan aantal incidenten. Hoogste waarden bij kills zijn januari en maart, waarbij de laagste
 #waarde april is. Hoogste waarden bij injuries zijn juli, januari en augustus die allen dicht bij elkaar zitten,
-#met als laagste waarden februari. Wat opvalt hierbij is dat de verhouding van doden en gewonden verschild per maand, 
-# februari is het laagste in gewonden, maar zit in de top 5 bij doden. 
+#met als laagste waarden februari. Wat opvalt hierbij is dat de verhouding van doden en gewonden verschild per maand,
+# februari is het laagste in gewonden, maar zit in de top 5 bij doden.
