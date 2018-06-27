435322f56e9c3d59d1221595b5360d665afb510d
bartvanvulpen99 <bartvanvulpen@icloud.com>
2018-06-27 16:12:18 +0200

    added killed injured by city

--- python_files/ploty_bar_chart_cities.py
+++ python_files/ploty_bar_chart_cities.py
@@ -22,47 +22,72 @@ plotly.tools.set_credentials_file(username='BartvanVulpen', api_key='f6VVqnMdHc9
 print("Starting reading data...")
 data = pd.read_excel('datasets/city_data.xlsx')
 data = data.sort_index()
+# trace1 = go.Bar(
+#     x= data['city'],
+#     y= data['robbery'],
+#     name='Robbery'
+# )
+# trace2 = go.Bar(
+#     x= data['city'],
+#     y= data['drug_involvement'],
+#     name='Drug involvement'
+# )
+#
+# trace3 = go.Bar(
+#     x= data['city'],
+#     y= data['accidental'],
+#     name='Accidental'
+# )
+# trace4 = go.Bar(
+#     x= data['city'],
+#     y= data['suicide'],
+#     name='Suicide'
+# )
+# trace5 = go.Bar(
+#     x= data['city'],
+#     y= data['gang_involvement'],
+#     name='Gang involvement'
+# )
+# trace6 = go.Bar(
+#     x= data['city'],
+#     y= data['officer_involved'],
+#     name='Officer involved'
+# )
+# trace7 = go.Bar(
+#     x= data['city'],
+#     y= data['domestic_violence'],
+#     name='Domestic violence'
+# )
+#
+# data = [trace1, trace2, trace3, trace4, trace5, trace6, trace7]
+# layout = go.Layout(
+#     barmode='group'
+# )
+#
+# fig = go.Figure(data=data, layout=layout)
+# py.iplot(fig, filename='grouped-bar')
+
+
 trace1 = go.Bar(
     x= data['city'],
-    y= data['robbery'],
-    name='Robbery'
+    y= data['n_injured'],
+    name='People killed'
 )
 trace2 = go.Bar(
     x= data['city'],
-    y= data['drug_involvement'],
-    name='Drug involvement'
+    y= data['n_killed'],
+    name='People injured'
 )
 
 trace3 = go.Bar(
     x= data['city'],
-    y= data['accidental'],
-    name='Accidental'
+    y= data['n_incidents'],
+    name='Number of incidents'
 )
-trace4 = go.Bar(
-    x= data['city'],
-    y= data['suicide'],
-    name='Suicide'
-)
-trace5 = go.Bar(
-    x= data['city'],
-    y= data['gang_involvement'],
-    name='Gang involvement'
-)
-trace6 = go.Bar(
-    x= data['city'],
-    y= data['officer_involved'],
-    name='Officer involved'
-)
-trace7 = go.Bar(
-    x= data['city'],
-    y= data['domestic_violence'],
-    name='Domestic violence'
-)
-
-data = [trace1, trace2, trace3, trace4, trace5, trace6, trace7]
+data = [trace1, trace2, trace3]
 layout = go.Layout(
     barmode='group'
 )
 
 fig = go.Figure(data=data, layout=layout)
-py.iplot(fig, filename='grouped-bar')
+py.iplot(fig, filename='stack')
